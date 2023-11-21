96e82bf4fc07c598bb9910dfc4d9097110f8fc9b
marc.sitze <marcsitze01@gmail.com>
2023-11-21 21:30:14 +0100

    feat: P10-1 as a developer i can setup frontend for platform-1.0

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,12 @@
 import express, { RequestHandler, Request } from "express";
 import * as path from "path";
-import morgan from 'morgan';
-import { createServer } from "http";
-import { Server } from "socket.io";
+import morgan from "morgan";
+// import swaggerUi from "swagger-ui-express";
+// import swaggerJsDoc from "swagger-jsdoc";
 
 import config from "./config";
-import { createNotification } from './sockets/notifications'
 
 const app: express.Application = express();
-
-const socketServer = createServer(app)
-export const io = new Server(socketServer)
 // import routes
 import indexRoute from "./routes/index";
 
@@ -20,8 +16,28 @@ app.use(express.json() as RequestHandler);
 app.use(morgan("combined") as RequestHandler);
 
 // Set view engine
-app.set('view engine', 'ejs');
-app.set('views', __dirname + '/views');
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+
+// const swaggerOptions = {
+//   definition: {
+//     openapi: "1.0.0",
+//     info: {
+//       title: "PLATEFORM API",
+//       version: "1.0.0",
+//       description: "PLATEFORM API",
+//     },
+//     servers: [
+//       {
+//         url: "http://localhost:3000",
+//       },
+//     ],
+//   },
+//   apis: ["./routes/*.js"],
+// };
+
+// const swaggerDocs = swaggerJsDoc(swaggerOptions);
+// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 // static folders
 // app.use(express.static(path.join(__dirname, "/public")));
@@ -29,41 +45,14 @@ app.use(express.static(path.join(__dirname, "../uploads")));
 
 //get timestamp
 app.use((req: any, res, next) => {
-    req.requestTime = new Date().toISOString();
-    console.log('req.ip: ', req.ip)
-    console.log(req.headers);
+  req.requestTime = new Date().toISOString();
+  console.log("req.ip: ", req.ip);
+  console.log(req.headers);
 
-    next()
+  next();
 });
 
 // Entry point routes
 app.use("/api", indexRoute);
 
-const users: any = [];
-
-io.on("connection", (socket) => {
-  console.log('IO connected...');
-
-  socket.on('connected', (data) => {
-    const newUser = {
-      socket,
-      id: data.id,
-      name: data.name
-    }
-    createNotification(socket);
-
-    users.push(newUser)
-    console.log('data: ', data)
-    // console.log('users: ', users)
-    socket.emit('connected', data)
-  })
-
-  socket.on('like', (data) => {
-    console.log('liked...')
-    console.log('like: ', data)
-    // createNotification(data);
-    io.emit(data.userId, data)
-  })
-})
-
-export default socketServer;
+export default app;
